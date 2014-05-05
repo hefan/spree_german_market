bb3047147f3d0c56df764651806f3f5305f3f16b
Stefan Hartmann <stefan@wemove.com>
2014-05-05 03:04:37 +0200

    rails 3 active record adaptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,25 @@ if germany.present?
     z.save!
   end
   
-  zone = Spree::Zone.find_or_create_by(name: "Deutschland")
+  zone = Spree::Zone.find_or_create_by_name("Deutschland")
   zone.description = "Deutschland"
   zone.default_tax = true
   zone.country_ids = [germany.id]
   zone.save!
 
-  tax_cat_1 = Spree::TaxCategory.find_or_create_by(name: "Standard") 
+  tax_cat_1 = Spree::TaxCategory.find_or_create_by_name("Standard") 
   tax_cat_1.deleted_at = false
   tax_cat_1.description = "Standard Mehrwertsteuer Deutschland"
   tax_cat_1.is_default = true
   tax_cat_1.save!
 
-  tax_cat_2 = Spree::TaxCategory.find_or_create_by(name: "Lebensmittel") 
+  tax_cat_2 = Spree::TaxCategory.find_or_create_by_name("Lebensmittel") 
   tax_cat_2.deleted_at = false
   tax_cat_2.description = "Lebensmittel Mehrwertsteuer Deutschland"
   tax_cat_2.is_default = false
   tax_cat_2.save!
 
-  tax_rate_1 = Spree::TaxRate.find_or_create_by(name: "inkl. 19% MwSt.")
+  tax_rate_1 = Spree::TaxRate.find_or_create_by_name("inkl. 19% MwSt.")
   tax_rate_1.deleted_at = false
   tax_rate_1.amount = 0.19
   tax_rate_1.zone = zone
@@ -36,7 +36,7 @@ if germany.present?
   tax_rate_1.calculator_type = "Spree::Calculator::DefaultTax"
   tax_rate_1.save!
 
-  tax_rate_2 = Spree::TaxRate.find_or_create_by(name: "inkl. 7% MwSt.")
+  tax_rate_2 = Spree::TaxRate.find_or_create_by_name("inkl. 7% MwSt.")
   tax_rate_2.deleted_at = false
   tax_rate_2.amount = 0.07
   tax_rate_2.zone = zone
@@ -47,9 +47,9 @@ if germany.present?
   tax_rate_2.save!
 
 
-  default_shipping_cat = Spree::ShippingCategory.find_or_create_by(name: "Standard Deutschland")
+  default_shipping_cat = Spree::ShippingCategory.find_or_create_by_name("Standard Deutschland")
 
-  shipping_method_1 = Spree::ShippingMethod.find_or_create_by(name: "Flat Deutschland") 
+  shipping_method_1 = Spree::ShippingMethod.find_or_create_by_name("Flat Deutschland") 
   shipping_method_1.deleted_at = false
   shipping_method_1.zones = [zone]
   shipping_method_1.shipping_categories = [default_shipping_cat]
