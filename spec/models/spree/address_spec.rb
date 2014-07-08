a5ceaa7c669c8dc9b63ee45233fb3dba5ac9a537
Stefan Hartmann <stefan@pfeffans.local>
2014-07-08 15:07:40 +0200

    changes for 2-3

--- spec/models/spree/address_spec.rb
+++ spec/models/spree/address_spec.rb
@@ -4,13 +4,13 @@ describe Spree::Address do
 
   it "address without street number fails" do
     adr = FactoryGirl.build(:address)
-    adr.save.should be_false
+    adr.save.should be false
   end
 
   it "address with street number works" do
     adr = FactoryGirl.build(:address)
     adr.street_number = "123"
-    adr.save.should be_true
+    adr.save.should be true
   end
 
 end
